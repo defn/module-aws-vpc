b785967716ab1d3eac2cdc0485d95a4c95e804bb
Tom Bombadil <amanibhavam@destructuring.org>
2016-06-14 20:37:44 -0700

    shareable variable definitions

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
-provider "aws" {
-}
+provider "aws" { }
 
 resource "aws_vpc" "vpc" {
   cidr_block = "${var.vpc_cidr}"
@@ -19,4 +18,20 @@ resource "aws_internet_gateway" "igw" {
     "Vpc" = "${var.vpc_name}"
     "Provisioner" = "tf"
   }
+}
+
+output "vpc_name" {
+  value = "${var.vpc_name}"
+}
+
+output "vpc_id" {
+  value = "${aws_vpc.vpc.id}"
+}
+
+output "vpc_cidr" {
+  value = "${aws_vpc.vpc.cidr}"
+}
+
+output "igw_id" {
+  value = "${aws_internet_gateway.igw.id}"
 }
